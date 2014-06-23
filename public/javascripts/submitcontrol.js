a2bfb29d8d2a792dec7c4733e423ee83203d16a3
FrontSide <drieger@gmx.at>
2014-06-23 20:47:46 +0200

    changed to bootstrap-wysihtml5 .. no more xss but editor doesnt grow

--- public/javascripts/submitcontrol.js
+++ public/javascripts/submitcontrol.js
@@ -22,17 +22,21 @@ $('#newentry_pub_inst_send').click(function () {
 
 /* Summernote HTML Editor (with costum toolbar) */
 $(document).ready(function() {
-  $('.summernote').summernote({
-      height: 100,
-      toolbar: [
-        //[groupname, [button list]]         
-        ['style', ['bold', 'italic', 'underline', 'clear']],
-        ['font', ['strikethrough']],
-        ['fontsize', ['fontsize']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-      ]
-  });
+
+  $('#post_content').wysihtml5({
+        "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+        "emphasis": true, //Italics, bold, etc. Default true
+        "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+        "html": false, //Button which allows you to edit the generated HTML. Default false
+        "link": true, //Button to insert a link. Default true
+        "image": true, //Button to insert an image. Default true,
+        "color": false, //Button to change color of font
+        "size": 'sm' //Button size like sm, xs etc.
+    }); 
+
+  $('#post_content').css("height", "auto");
+  $('#post_content').resizable({handles: "se"});
+
 });
 
 
