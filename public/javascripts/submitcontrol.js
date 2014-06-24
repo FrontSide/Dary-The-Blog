1616e2a725959219d6e85f5db70be70b1a7fe068
FrontSide <drieger@gmx.at>
2014-06-24 11:02:27 +0200

    use ckeditor - not really lightweight but meh

--- public/javascripts/submitcontrol.js
+++ public/javascripts/submitcontrol.js
@@ -20,23 +20,10 @@ $('#newentry_pub_inst_send').click(function () {
 
 }); 
 
-/* Summernote HTML Editor (with costum toolbar) */
+/* CK HTML Editor */
 $(document).ready(function() {
-
-  $('#post_content').wysihtml5({
-        "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
-        "emphasis": true, //Italics, bold, etc. Default true
-        "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
-        "html": false, //Button which allows you to edit the generated HTML. Default false
-        "link": true, //Button to insert a link. Default true
-        "image": true, //Button to insert an image. Default true,
-        "color": false, //Button to change color of font
-        "size": 'sm' //Button size like sm, xs etc.
-    }); 
-
-  $('#post_content').css("height", "auto");
-  $('#post_content').resizable({handles: "se"});
-
+  CKEDITOR.replace( 'post_content' );
 });
+
 
 
