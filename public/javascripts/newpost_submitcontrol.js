f12f51d20c53452d70f631610c9398d19d87cdd6
FrontSide <drieger@gmx.at>
2014-06-29 16:11:09 +0200

    design update

--- public/javascripts/newpost_submitcontrol.js
+++ public/javascripts/newpost_submitcontrol.js
@@ -2,32 +2,32 @@
 /* Markdown is converted to HTML on save */
 
 /* Publish NOW Button - Opens Modal */
-$('#newentry_pub_inst_btn').click(function () {
+$('#newpost_pub_inst_btn').click(function () {
 
     console.log("publish post...")
-    $('#newentry_pub_inst_modal').modal('show')
+    $('#newpost_pub_inst_modal').modal('show')
 
 });
 
 /* Save Entry and Publish now - From Modal Dialog 
     Submit Form */
-$('#newentry_pub_inst_send').click(function () {
+$('#newpost_pub_inst_send').click(function () {
 
     /* Convert */
     $('#content').val(markdown.toHTML($('#content').val()));
 
     /* If "Absolutely" Button clicked.. submit form with
        "publishNow" variable set true */
-    $('#newenty_hidden_publish').val("true");
-    $("#newentry_form").submit();
+    $('#newpost_hidden_publish').val("true");
+    $("#newpost_form").submit();
 
 }); 
 
 /* Save Without Publish Button */
-$('#newentry_save_btn').click(function () {
+$('#newpost_save_btn').click(function () {
   /* Convert */
   $('#content').val(markdown.toHTML($('#content').val()));
-  $("#newentry_form").submit();
+  $("#newpost_form").submit();
 });
 
 /* On Load */
