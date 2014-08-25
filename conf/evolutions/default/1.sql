9ca521a9f30798b9f4b6da38923f1aef1319fa8c
drieger <drieger@gmx.at>
2014-08-25 16:57:01 +0200

    apparently ebean ddl generation needs to be turned off

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -6,7 +6,7 @@
 create table article_comment (
   id                        bigint not null,
   post_id                   bigint,
-  content                   text,
+  content                   clob,
   user_id                   bigint,
   is_deleted                boolean,
   cre_date                  timestamp,
@@ -22,7 +22,7 @@ create table picture (
 create table post (
   id                        bigint not null,
   title                     varchar(255),
-  content                   text,
+  content                   clob,
   user_id                   bigint,
   cre_date                  timestamp,
   is_published              boolean,
@@ -61,34 +61,38 @@ create sequence blog_user_seq;
 
 create sequence user_log_seq;
 
-alter table article_comment add constraint fk_article_comment_post_1 foreign key (post_id) references post (id);
+alter table article_comment add constraint fk_article_comment_post_1 foreign key (post_id) references post (id) on delete restrict on update restrict;
 create index ix_article_comment_post_1 on article_comment (post_id);
-alter table article_comment add constraint fk_article_comment_user_2 foreign key (user_id) references blog_user (id);
+alter table article_comment add constraint fk_article_comment_user_2 foreign key (user_id) references blog_user (id) on delete restrict on update restrict;
 create index ix_article_comment_user_2 on article_comment (user_id);
-alter table picture add constraint fk_picture_user_3 foreign key (user_id) references blog_user (id);
+alter table picture add constraint fk_picture_user_3 foreign key (user_id) references blog_user (id) on delete restrict on update restrict;
 create index ix_picture_user_3 on picture (user_id);
-alter table post add constraint fk_post_user_4 foreign key (user_id) references blog_user (id);
+alter table post add constraint fk_post_user_4 foreign key (user_id) references blog_user (id) on delete restrict on update restrict;
 create index ix_post_user_4 on post (user_id);
-alter table post add constraint fk_post_rootPost_5 foreign key (root_post_id) references post (id);
+alter table post add constraint fk_post_rootPost_5 foreign key (root_post_id) references post (id) on delete restrict on update restrict;
 create index ix_post_rootPost_5 on post (root_post_id);
-alter table blog_user add constraint fk_blog_user_profilePicture_6 foreign key (profile_picture_id) references picture (id);
+alter table blog_user add constraint fk_blog_user_profilePicture_6 foreign key (profile_picture_id) references picture (id) on delete restrict on update restrict;
 create index ix_blog_user_profilePicture_6 on blog_user (profile_picture_id);
-alter table user_log add constraint fk_user_log_user_7 foreign key (user_id) references blog_user (id);
+alter table user_log add constraint fk_user_log_user_7 foreign key (user_id) references blog_user (id) on delete restrict on update restrict;
 create index ix_user_log_user_7 on user_log (user_id);
 
 
 
 # --- !Downs
 
-drop table if exists article_comment cascade;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists picture cascade;
+drop table if exists article_comment;
 
-drop table if exists post cascade;
+drop table if exists picture;
 
-drop table if exists blog_user cascade;
+drop table if exists post;
 
-drop table if exists user_log cascade;
+drop table if exists blog_user;
+
+drop table if exists user_log;
+
+SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists article_comment_seq;
 
